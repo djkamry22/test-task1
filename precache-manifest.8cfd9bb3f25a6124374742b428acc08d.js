e8741ea9170ae195b53ab102b54eadcc6e115a77
Kamran <djkamry@gmail.com>
2020-02-11 18:16:10 +0200

    Updates

---
+++ precache-manifest.8cfd9bb3f25a6124374742b428acc08d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4693d66317a7e4183fb1074af0019591",
+    "url": "/guru-task/index.html"
+  },
+  {
+    "revision": "515afe38658096771e75",
+    "url": "/guru-task/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "de7f5064f2fbfe4c2b62",
+    "url": "/guru-task/static/css/main.283ae003.chunk.css"
+  },
+  {
+    "revision": "515afe38658096771e75",
+    "url": "/guru-task/static/js/2.feba1123.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/guru-task/static/js/2.feba1123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7f5064f2fbfe4c2b62",
+    "url": "/guru-task/static/js/main.67129032.chunk.js"
+  },
+  {
+    "revision": "ad0fd5edaa60c2a20c7c",
+    "url": "/guru-task/static/js/runtime-main.1e78a34f.js"
+  }
+]);
